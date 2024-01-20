9788939bc63f9dd778e30f644627ef3159699ab7
VitorDevPlay <vitor.abreu@playmovel.com.br>
2024-01-20 19:54:12 -0300

    alteração do butão

--- src/screens/Security.tsx
+++ src/screens/Security.tsx
@@ -1,7 +1,7 @@
 import { Box, FlatList, Flex, Heading, Switch, Text, Toast } from "native-base";
 import React, { useCallback, useEffect } from "react";
 import { Header } from "../components/Header";
-import { changeIcon, getIcon,  } from "react-native-change-icon";
+import { changeIcon } from "react-native-change-icon";
 import { useFocusEffect } from "@react-navigation/native";
 import { Platform, AppState } from "react-native";
 import { dataApps } from "../mocks/dataApps";
@@ -19,50 +19,51 @@ interface IAppItemProps {
 
 export default function Security() {
   const [currentIcon, setCurrentIcon] = React.useState("");
-
   const [appState, setAppState] = React.useState(AppState.currentState);
 
-  const _handleAppStateChange = (nextAppState: any) => {
+  const handleAppStateChange = (nextAppState: any) => {
     if (appState.match(/inactive|background/) && nextAppState === "active") {
       console.log("App has come to the foreground!");
     }
     setAppState(nextAppState);
   };
+
+  useFocusEffect(
+    useCallback(() => {
+      AppState.addEventListener("change", handleAppStateChange);
+  
+      // Realize um type casting para 'any' para evitar o erro de compilação
+      return () => {
+        (AppState as any).removeEventListener("change", handleAppStateChange);
+      };
+    }, [appState])
+  );
+  
+
   const handleChangeLocation = async (value: boolean, props: IAppItemProps) => {
-  
     console.log(value);
-  
-    if (value) {
-      console.log(props.item.iconName);
-  
-      try {
+
+    try {
+      if (value) {
         await changeIcon(props.item.iconName);
         setCurrentIcon(props.item.iconName);
         Toast.show({
           title: "Icon changed",
           duration: 2000,
         });
-      } catch (error) {
-        console.error("Error changing icon:", error);
-        // Add specific error handling logic here if needed.
-      }
-    } else {
-      // If you want to restore the default icon, provide the default name.
-      changeIcon("default")
-        .then(() => {
-          setCurrentIcon("default");
-          Toast.show({
-            title: "Icon restored",
-            duration: 2000,
-          });
-        })
-        .catch((error) => {
-          console.error("Error restoring default icon:", error);
-          // Add specific error handling logic here if needed.
+      } else {
+        await changeIcon("default");
+        setCurrentIcon("default");
+        Toast.show({
+          title: "Icon restored",
+          duration: 2000,
         });
+      }
+    } catch (error) {
+      console.error("Error changing icon:", error);
+      // Adicione lógica de tratamento de erro específica aqui, se necessário.
     }
   };
-  
 
   const AppItem = (props: IAppItemProps) => {
     return (
@@ -92,7 +93,7 @@ export default function Security() {
           <Switch
             value={currentIcon === props.item.iconName}
             onValueChange={async (value) => {
-              handleChangeLocation(value, props)
+              handleChangeLocation(value, props);
             }}
           />
         </Flex>
